f3ae9c6c2be3f57ec570c0abb0f20032ebf9824a
Joren l'Ami <joren@distinct.nl>
2020-01-17 11:07:07 +0100

    createClassComputed and totalPages

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -3,6 +3,7 @@ import {computed} from '@ember/object';
 import {alias} from '@ember/object/computed';
 import {A} from '@ember/array';
 import ArrayProxy from '@ember/array/proxy';
+import { createClassComputed } from 'ember-macro-helpers';
 
 const PagedArray = ArrayProxy.extend({
   perPage: 2,
@@ -13,10 +14,14 @@ const PagedArray = ArrayProxy.extend({
     let result = this.get('content').slice((page-1)*perPage, page*perPage);
     return result;
   }),
+  
+  totalPages: computed("content.length", "perPage", function() {
+    return Math.ceil(this.get('content.length')/this.get("perPage"));
+  }),
 });
 
 function pagedArray(modelName, pageName) {
-  return computed("", function() {
+  return computed(function() {
     let instanceOpts = {
       parent: this
     };
@@ -32,11 +37,21 @@ function pagedArray(modelName, pageName) {
   });
 }
 
+const pagedArrayClassComputed = createClassComputed([false, false], function(modelName, pageName) {
+  return computed(modelName + ".[]", pageName, function() {
+    let page = this.get(pageName);
+    let perPage = 2;//this.get('perPage');
+    let result = this.get(modelName).slice((page-1)*perPage, page*perPage);
+    return result;
+  });
+});
+
 export default Controller.extend({
   model: A([1, 2, 3, 4, 5, 6, 7]),
   page: 1,
   
   pagedContent: pagedArray('model', 'page'),
+  pagedContent2: pagedArrayClassComputed('model', 'page'),
   
   actions: {
     next() {
